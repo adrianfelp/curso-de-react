07f62b9264ff43b76fa8afd7ed1eea4449745790
Adrian Felipe <adrianfelipepereira@gmail.com>
2026-05-23 17:27:05 -0300

    ex useState ainda não finalizado

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,14 @@
 import Titulo from "./Titulo"
 function App() {
-  return <Titulo />
+  return (
+    <div>
+      <Titulo cor = 'red' />
+      <Titulo cor = 'blue' />
+      <Titulo cor = 'purple' />
+      <Titulo cor = 'orangered'/>
+    </div>
+
+  )
 }
 
 export default App
--- vite-project/src/Titulo.jsx
+++ vite-project/src/Titulo.jsx
@@ -1,5 +1,18 @@
-function Titulo() {
-    return <h1>Sou um Título</h1>
+import { useState } from "react"
+function Titulo({cor}) {
+    const [texto, setTexto] = useState('Um título do estado inicial')
+    const [inputText, setInputText] = useState('')
+
+    function clicou() {
+        setTexto(inputText)
+    }
+    return (
+    <div>
+        <h1 style={{color: cor}}>{texto}</h1>
+        <input value={inputText} onChange={(e)=>{setInputText(e.target.value)}} type="text" />
+        <button onClick={clicou}>Mudar</button>
+    </div>
+    )
 }
 
 export default Titulo
