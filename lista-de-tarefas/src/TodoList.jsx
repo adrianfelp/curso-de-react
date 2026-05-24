5d6f49b510b529c9202f247f40f279a11075bac9
Adrian Felipe <adrianfelipepereira@gmail.com>
2026-05-24 17:15:25 -0300

    lista de tarefas iniciado

---
+++ lista-de-tarefas/src/TodoList.jsx
@@ -0,0 +1,27 @@
+import React from 'React';
+import './TodoList.css';
+
+function TodoList(){
+    return (
+        <div>
+            <h1>Lista de Tarefas</h1>
+            <form>
+                <input type="text" placeholder='Adicione uma tarefa'/>
+                <button className="add" type="submit">Add</button>
+            </form>
+            <div className='listaTarefas'>
+                <div className='item'>
+                    <span>Tarefa de exemplo</span>
+                    <button>Deletar</button>
+                </div>
+                <div className='item completo'>
+                    <span>Tarefa de exemplo</span>
+                    <button>Deletar</button>
+                </div>
+            </div>
+        </div>
+        
+    )
+}
+
+export default TodoList
