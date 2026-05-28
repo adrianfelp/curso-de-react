701710149aa6a4ec7bc0b9a762e335337526bfc7
Adrian Felipe <adrianfelipepereira@gmail.com>
2026-05-28 10:23:31 -0300

    lista de tarefas finalizada

--- lista-de-tarefas/src/TodoList.jsx
+++ lista-de-tarefas/src/TodoList.jsx
@@ -1,24 +1,85 @@
-import React from 'React';
+import React, { useState, useEffect } from 'react';
 import './TodoList.css';
+import Icon from './assets/icon-png.webp'
 
 function TodoList(){
+
+    const listaStorage = localStorage.getItem('Lista')
+
+    const [lista, setLista] = useState(listaStorage ? JSON.parse(listaStorage) : [])
+    const[novoItem, setNovoItem] = useState('')
+
+    useEffect(()=>{
+        localStorage.setItem('Lista', JSON.stringify(lista))
+    },[lista])
+
+    function adicionaItem(form) {
+        form.preventDefault()
+        if(!novoItem) {
+            return
+        }
+        setLista([...lista, {text: novoItem, isCompleted:false}])
+        setNovoItem('')
+        document.getElementById('input-entrada').focus()
+    }
+
+    function clicou (index) {
+        const listaAux = [...lista]
+        listaAux[index].isCompleted = !listaAux[index].isCompleted
+        setLista(listaAux)
+    }
+
+    function deleta(index) {
+        const listaAux = [...lista]
+        listaAux.splice(index,1)
+        setLista(listaAux)
+    }
+
+    function deletatudo() {
+        setLista([])
+    }
+
     return (
         <div>
             <h1>Lista de Tarefas</h1>
-            <form>
-                <input type="text" placeholder='Adicione uma tarefa'/>
+            <form onSubmit={adicionaItem}>
+                <input 
+                    id='input-entrada'
+                    type="text" 
+                    value={novoItem}
+                    onChange={(e)=>{setNovoItem(e.target.value)}}
+                    placeholder='Adicione uma tarefa'/>
                 <button className="add" type="submit">Add</button>
             </form>
             <div className='listaTarefas'>
-                <div className='item'>
-                    <span>Tarefa de exemplo</span>
-                    <button>Deletar</button>
-                </div>
-                <div className='item completo'>
-                    <span>Tarefa de exemplo</span>
-                    <button>Deletar</button>
-                </div>
+             <div style={{ textAlign: 'center' }}>
+                {
+                    lista.length <1
+                    ?
+                    <img className='icon-central' src={Icon}/>
+                    :
+                    lista.map((item, index) => (
+                        <div 
+                        key={index}
+                        className={item.isCompleted ? 'item completo' : 'item'}>
+                            <span onClick={()=>{clicou(index)}}>{item.text}</span>
+                            <button  
+                            className='del'
+                            onClick={()=>{deleta(index)}}
+                            >
+                                Deletar
+                            </button>
+                        </div>
+                    ))
+        
+                }
+                {
+                    lista.length > 0 &&
+                    <button onClick={()=>{deletatudo()}} className='delall'>Deletar Todas</button>
+                }
+                
             </div>
+         </div>
         </div>
         
     )
